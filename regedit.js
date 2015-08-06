a94a8f2486607a0570a4698cda0659aa38d2d9a0
Alnyli07 <alitugrulpinar@yandex.com>
2015-08-07 01:05:20 +0300

    implented getjavaHome TO DO ignorelist implemented.

--- regedit.js
+++ regedit.js
@@ -1,5 +1,6 @@
 var spawn = require('child_process').spawn;
 var path = require('path');
+var os = require('os')
 
 var _ = require('underscore');
 
@@ -45,18 +46,20 @@ function searchPaths(firstPath, searched, callBack) {
 	var dataFounded = [];
 	var errorFounded = [];
 
-	function searchPath(firstPath, searched) {
+	var ignoreList = ['microsoft', 'Wow6432Node', 'Classes']; // FIX will be implented to take as a paramaeter.
+
+	function searchPath(firstPath, searched) { // searpath recursively.
 
 		if (firstPath && firstPath.split('    ').length === 1 && firstPath !== '') {
-			var child = spawn('reg', ['QUERY', firstPath.replace(/\//gm, path.sep)]);
-			++activeChild;
+			var child = spawn('reg', ['QUERY', firstPath.replace(/\//gm, path.sep)]); // run reg command.
+			++activeChild; // running child.
 			var bufferStdOut = new Buffer('');
 			var bufferStdErr = new Buffer('');
 			child.stdout.on('data', function(data) {
 				bufferStdOut += data;
 			});
 			child.on('close', function(code) {
-				++closedChild;
+
 				if (code === 0) {
 					var dataStr = bufferStdOut.toString('utf8');
 					var splitData = dataStr.split('\r\n');
@@ -64,7 +67,22 @@ function searchPaths(firstPath, searched, callBack) {
 					if (res === null) {
 						_.each(splitData, function(item) {
 							if (item !== firstPath) {
-								searchPath(item, searched);
+								// console.log(item); //debug
+								if (ignoreList) {
+									var ignored = false;
+									_.each(ignoreList, function(ignore) {
+										if (!ignored) {
+											if (item.search(new RegExp(ignore, 'gmi')) !== -1) {
+												ignored = true;
+											}
+										}
+									});
+									if (!ignored) {
+										searchPath(item, searched);
+									}
+								} else {
+									searchPath(item, searched);
+								}
 							}
 						});
 
@@ -77,12 +95,12 @@ function searchPaths(firstPath, searched, callBack) {
 						msg: bufferStdErr.toString('utf8')
 					});
 				}
+				++closedChild; // closed child.
 				if (activeChild === closedChild) {
-					if (errorFounded.length === 0) {
+					if (errorFounded.length === 0 || dataFounded.length !== 0) {
 						callBack(null, dataFounded);
 					} else {
-
-						callBack(errorFounded, dataFounded);
+						callBack(errorFounded);
 					}
 				}
 			});
@@ -96,17 +114,9 @@ function searchPaths(firstPath, searched, callBack) {
 	searchPath(firstPath, searched);
 }
 
-searchPaths('HKLM/SOFTWARE/JavaSoft', 'JavaHome', function(err, data) {
-	if (err) {
-
-	} else {
-		if (data) {
-			console.dir(data);
-		}
-	}
-});
 
 function controlVersion(outputJava, javaVersion) { // control java version.
+	javaVersion = String(javaVersion);
 	var items = javaVersion.split('.');
 	var searched = javaVersion;
 	if (items.length === 2) {
@@ -120,9 +130,8 @@ function controlVersion(outputJava, javaVersion) { // control java version.
 	return res;
 }
 
-function controlJavaVersion(version, callBack) {
-	var child = spawn('java', ['-version']);
-
+function controlJavaVersion(javaPath, version, callBack) {
+	var child = spawn(javaPath, ['-version']);
 	var bufferStdOut = new Buffer('');
 	var bufferStdErr = new Buffer('');
 	child.stdout.on('data', function(data) {
@@ -131,9 +140,9 @@ function controlJavaVersion(version, callBack) {
 
 	child.on('close', function(code) {
 		if (controlVersion((bufferStdOut + bufferStdErr).toString('utf8'), version)) {
-			callBack(true);
+			callBack(javaPath);
 		} else {
-			callBack(false);
+			callBack(null);
 		}
 	});
 
@@ -142,13 +151,126 @@ function controlJavaVersion(version, callBack) {
 	});
 }
 
-controlJavaVersion('1.8', function(found) {
-	if (found) {
-		console.log('Java ' + 1.8 + ' bulundu.');
-	} else {
-		console.log('Java ' + 1.8 + ' bulunamadi.');
-	}
-})
 
+// search regs with JavaHome tickets.
+function searchJavaHome(root, version, callBack) {
+	var platform = os.platform(); // get platform.
+	if (platform === 'win32') { // check windows platform
+		searchPaths(root, 'JavaHome', function(err, data) { // x64
+			if (err) {
+				callBack(err);
+			} else if (data) {
+				var founded = false;
+				var countSearch = 0;
+				var errorMessage = 'Not found Java by the version ' + version;
+				for (var i = 0; i < data.length; i++) { // control version in results.
+					var javaHomeValue = data[i].value;
+					if (javaHomeValue.search(new RegExp(version, 'gmi')) !== -1) {
+						//console.log(javaHomeValue); // debug
+						controlJavaVersion(javaHomeValue + '\\bin\\java', version, function(path) { // control version item.
+							++countSearch;
+							if (!founded) { // if founded not to do.
+								if (path) { //if found path call callBack.
+									founded = true;
+									callBack(null, path);
+									return;
+								}
+								if (countSearch === data.length) {
+									callBack({
+										err: 'Found Error',
+										msg: errorMessage
+									});
+								}
+							}
+						});
+					} else {
+						++countSearch;
+					}
+
+				}
+				// if all or  some miss call controlJavaVersion, we can handle to call callBack.
+				if (countSearch === data.length) {
+					callBack({
+						err: 'Found Error',
+						msg: errorMessage + ' 2'
+					});
+				}
+
+			}
+
+		});
+	} else { // not supported platform.
+		callBack({
+			err: 'OS Platform',
+			msg: platform + 'doesn\'t support ! --> only windows platform'
+		});
+	}
+}
+
+//search roots that we can find javaHome with version
+//throws syntax error if callBack is not a function. 
+function getJavaHome(version, callBack, option) {
+	var roots = [];
+	if (option && option.root) {
+		roots.push(option.root);
+	} else {
+		roots.push('HKLM/SOFTWARE/JavaSoft');
+		roots.push('HKLM/SOFTWARE/Wow6432Node/JavaSoft');
+		roots.push('HKCU/SOFTWARE/JavaSoft');
+		roots.push('HKCU/SOFTWARE/Wow6432Node/JavaSoft');
+	}
+	if (typeof callBack !== 'function') {
+		var err = {
+			err: 'Syntax Error',
+			msg: 'check usage --> (version, callBack [ , option ] ) '
+
+		};
+		throw err;
+	}
+
+	var founded = false;
+	var countSearch = 0;
+	for (var i = 0; i < roots.length; i++) {
+		searchJavaHome(roots[i], version, function(err, data) {
+			++countSearch;
+			if (!founded) {
+				if (data) {
+					founded = true; // found correct path.
+					callBack(null, data);
+				}
+			}
+			if (countSearch === roots.length) {
+
+				if (!founded) {
+					callBack({
+						err: 'Found Error',
+						msg: 'Not found by the version ' + version
+					});
+				}
+			}
+		});
+		if (founded) {
+			break;
+		}
+	}
+
+}
+
+
+getJavaHome('1.7.0_79', function(err, data) {
+	if (err) {
+		console.log(err.msg);
+	}
+	if (data) {
+		console.log('DATA: ' + data);
+	}
+}, {
+	root: 'HKLM/SOFTWARE'
+});
+
+
+
+
+exports.getJavaHome = getJavaHome;
 exports.controlJavaVersion = controlJavaVersion;
 exports.search = searchPaths;
