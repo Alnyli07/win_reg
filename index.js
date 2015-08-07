d8961040d4f18dacd0ffbb18964a0d0f8f101522
ali tuğrul pınar <alitugrulpinar@yandex.com>
2015-08-07 12:01:14 +0300

    FIX lib and getjavaHome

---
+++ index.js
@@ -0,0 +1,21 @@
+var reg = require('./lib/regedit');
+
+/*
+reg.getJavaHome(1.7, function(err, data) {
+	if (err) {
+		console.log(err.msg);
+	}
+	if (data) {
+		console.log(data);
+	}
+});
+*/
+
+reg.search('HKLM/SOFTWARE', 'Enable', function(err, data) {
+	if (err) {
+		console.dir(err.msg);
+	}
+	if (data) {
+		console.dir(data);
+	}
+}, ['microsoft', 'Classes', 'Wow6432Node']);
